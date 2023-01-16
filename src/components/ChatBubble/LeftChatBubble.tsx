f465bff6e9b1ecf6bdb3cb04f7ddc280e8c56f2c
Flynn Foley-Clarke <mastare.flynn@gmail.com>
2023-01-16 16:53:25 +0000

    small styling change

--- src/components/ChatBubble/LeftChatBubble.tsx
+++ src/components/ChatBubble/LeftChatBubble.tsx
@@ -21,7 +21,10 @@ const LeftChatBubble = (props: messageProps) => {
           Obi-Wan
           <time className="text-xs opacity-50">12:45</time>
         </div>
-        <div className="chat-bubble">You were the Chosen One!</div>
+        <div className="chat-bubble">
+          You were the Chosen One! You were the Chosen One! You were the Chosen
+          One! You were the Chosen One! You were the Chosen One!
+        </div>
         <div className="chat-footer opacity-50">Delivered</div>
       </div>
     </>
--- src/components/ChatBubble/RightChatBubble.tsx
+++ src/components/ChatBubble/RightChatBubble.tsx
@@ -13,7 +13,7 @@ const RightChatBubble = () => {
         <time className="text-xs opacity-50">12:46</time>
       </div>
       <div className="chat-bubble">I hate you!</div>
-      <div className="chat-footer opacity-50">Seen at 12:46</div>
+      <div className="chat-footer opacity-50">Delivered</div>
     </div>
   );
 };
