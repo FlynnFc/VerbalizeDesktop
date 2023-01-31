3f61c6a580f8ab368dddee88156896b47e97f978
Flynn Foley-Clarke <mastare.flynn@gmail.com>
2023-01-31 16:26:59 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
+  safelist: ["border-green-500", "border-yellow-400", "border-base-300"],
   daisyui: {
     themes: ["winter"],
   },
